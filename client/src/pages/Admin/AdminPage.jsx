11bb3bdcec1d9c6f497b6ce9d5cf6c50c311aa23
Lucas <jeanlucas84@outlook.com>
2025-03-20 18:44:30 +0100

    add restraint route crud with url

--- client/src/pages/Admin/AdminPage.jsx
+++ client/src/pages/Admin/AdminPage.jsx
@@ -1,24 +1,37 @@
-import { useGetAllPastryQuery } from "../../store/slice/pastrySlice"
-
+import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
+
 import { selectPastryForm } from "../../store/selectors/form-selector"
 import { setPastryForm, setPastryIsUpdate } from "../../store/slice/formSlice"
+import { useGetAllPastryQuery } from "../../store/slice/pastrySlice"
+import { useGetUserQuery } from "../../store/slice/userSlice"
 
 import Button from "../../components/Button"
 import GridPastry from "../../components/GridPastry"
 import FormPastry from "../../components/FormPastry"
 
 import "../../assets/style/admin.scss"
+import { useNavigate } from "react-router"
 
 const AdminPage = () => {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
+
     const { data, isLoading, isError, isSuccess } = useGetAllPastryQuery()
     const { showForm } = useSelector(selectPastryForm)
+
+    const { isSuccess: isConnected, isLoading: waiting } = useGetUserQuery()
 
     const handleClick = () => {
         dispatch(setPastryIsUpdate(false))
         dispatch(setPastryForm(true))
     }
+
+    useEffect(() => {
+        if (!waiting && !isConnected) {
+            navigate("/login")
+        }
+    }, [waiting, isConnected, navigate])
 
     return (
         <div className="container">
